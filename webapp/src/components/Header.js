82d101d009b1e801bba35b788f5f9907eb337cb1
annabrisland <84166964+annabrisland@users.noreply.github.com>
2024-02-14 20:01:28 -0800

    Adds working navigation

--- webapp/src/components/Header.js
+++ webapp/src/components/Header.js
@@ -1,83 +1,45 @@
 import nameLogo from "../assets/images/nameLogo.png";
-import logo from "../assets/images/logo.png"
+import logo from "../assets/images/logo.png";
 import "../assets/css/header.css";
-import { useHistory } from "react-router-dom";
+import resume from "../assets/Anna_Brisland_Resume.pdf";
+import { useState } from "react";
 
-export default function Header({ isProject, isMobile }) {
-  function openOverlay() {
-    document.getElementById("about-overlay").style.height = "100%";
-    document.body.style.overflow = "hidden";
-    document.getElementById("about").className = "nav-item about slide-left";
-  }
+export default function Header({ isMobile }) {
 
-  function closeOverlay() {
-    document.getElementById("about-overlay").style.height = "0%";
-    document.body.style.overflow = "";
-    document.getElementById("about").className = "nav-item about slide-right";
-  }
+  let currentPage = "work";
+  if (window.document.URL.includes("about")) {
+    currentPage = "about";
+  } else if (window.document.URL.includes("contact")){
+    currentPage = "contact"
+  };
 
+  const [ activeLink, setActiveLink ] = useState(currentPage);
+ 
   if (isMobile) {
-  }
-
-  if (isProject) {
-    return (
-      <div>
-        <nav className="main-header">
-          <a className="logo" href="#"></a>
-          <div className="nav-items">
-            <div className="nav-item about" id="about" onClick={openOverlay}>
-              About
-            </div>
-            <div className="nav-item CTA">Resume</div>
-          </div>
+    <nav className="floating">
+          <ul className="header-menu-list">
+            <li className="header-menu-item">
+              <a className={`header-menu-link ${activeLink == "work" ? "header-menu-current" : ""}`} href="/" >
+                Work
+              </a>
+            </li>
+            <li className="header-menu-item">
+              <a className={`header-menu-link ${activeLink == "about" ? "header-menu-current" : ""}`} href="/about" >
+                About
+              </a>
+            </li>
+            <li className="header-menu-item">
+              <a className={`header-menu-link ${activeLink == "contact" ? "header-menu-current" : ""}`} href="/contact" >
+                Contact
+              </a>
+            </li>
+            <li className="header-menu-item">
+              <a className="header-menu-link" href={resume} target="_blank">
+                Resume
+              </a>
+            </li>
+          </ul>
         </nav>
-        <div className="overlay" id="about-overlay">
-          <button className="close-btn" onClick={closeOverlay}></button>
-          <div className="overlay-content content grid">
-            <div className="about-paragraph">
-              <div className="main-paragraph">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                Suspisse dictum ex non purus cursus tempor.
-              </div>
-              <div className="second-paragraph">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                Suspisse dictum ex non purus cursus tempor.
-              </div>
-            </div>
-            <div className="about-skills">
-              Skills
-              <ul className="skills-list">
-                <li className="skill">HTML</li>
-                <li className="skill">CSS</li>
-                <li className="skill">JavaScript</li>
-                <li className="skill">JQuery</li>
-                <li className="skill">Bootstrap</li>
-                <li className="skill">Figma</li>
-                <li className="skill">Adobe Creative Cloud</li>
-              </ul>
-            </div>
-            <div className="about-contact">
-              Contact
-              <ul className="contact-list">
-                <li className="contact">
-                  <a href="mailto:annabrisland@gmail.com">Email</a>
-                </li>
-                <li className="contact">
-                  <a href="https://github.com/annabrisland" target="_blank">
-                    GitHub
-                  </a>
-                </li>
-                <li className="contact">
-                  <a href="www.linkedin.com/in/anna-brisland" target="_blank">
-                    Linkedin
-                  </a>
-                </li>
-              </ul>
-            </div>
-          </div>
-        </div>
-      </div>
-    );
   }
 
   return (
@@ -143,29 +105,34 @@ export default function Header({ isProject, isMobile }) {
     <header>
       <div className="header-items">
         <a>
-        <img className="name-logo" src={nameLogo}></img>
+          <img className="name-logo" src={nameLogo}></img>
         </a>
         <nav className="floating">
           <ul className="header-menu-list">
             <li className="header-menu-item">
-              <a className="header-menu-link" href="/about">
+              <a className={`header-menu-link ${activeLink == "work" ? "header-menu-current" : ""}`} href="/" >
+                Work
+              </a>
+            </li>
+            <li className="header-menu-item">
+              <a className={`header-menu-link ${activeLink == "about" ? "header-menu-current" : ""}`} href="/about" >
                 About
               </a>
             </li>
             <li className="header-menu-item">
-              <a className="header-menu-link" href="/contact">
+              <a className={`header-menu-link ${activeLink == "contact" ? "header-menu-current" : ""}`} href="/contact" >
                 Contact
               </a>
             </li>
             <li className="header-menu-item">
-              <a className="header-menu-link" href="/resume">
+              <a className="header-menu-link" href={resume} target="_blank">
                 Resume
               </a>
             </li>
           </ul>
         </nav>
         <div>
-        <img className="header-logo" src={logo}></img>
+          <img className="header-logo" alt="" src={logo}></img>
         </div>
       </div>
     </header>
