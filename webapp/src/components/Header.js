b87731d82ab5e6d606941bc9a98b187871aa6ff2
annabrisland <84166964+annabrisland@users.noreply.github.com>
2024-02-24 15:53:51 -0800

    Tidies footer css

--- webapp/src/components/Header.js
+++ webapp/src/components/Header.js
@@ -38,65 +38,6 @@ export default function Header({ isMobile }) {
   }
 
   return (
-    // <div>
-    //   <nav className="main-header">
-    //     <div className="intro">
-    //       <img className="name-logo" src={nameLogo}></img>
-    //       <p className="name-title">Web Designer & Developer</p>
-    //     </div>
-    //     <div className="nav-items">
-    //       <div className="nav-item about" id="about" onClick={openOverlay}>
-    //         About
-    //       </div>
-    //       <div className="nav-item CTA">Resume</div>
-    //     </div>
-    //   </nav>
-    //   <div className="overlay" id="about-overlay">
-    //     <button className="close-btn" onClick={closeOverlay}></button>
-    //     <div className="overlay-content content grid">
-    //       <div className="about-paragraph">
-    //         <div className="main-paragraph">
-    //           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspisse
-    //           dictum ex non purus cursus tempor.
-    //         </div>
-    //         <div className="second-paragraph">
-    //           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspisse
-    //           dictum ex non purus cursus tempor.
-    //         </div>
-    //       </div>
-    //       <div className="about-skills">
-    //         Skills
-    //         <ul className="skills-list">
-    //           <li className="skill">HTML</li>
-    //           <li className="skill">CSS</li>
-    //           <li className="skill">JavaScript</li>
-    //           <li className="skill">JQuery</li>
-    //           <li className="skill">Bootstrap</li>
-    //           <li className="skill">Figma</li>
-    //           <li className="skill">Adobe Creative Cloud</li>
-    //         </ul>
-    //       </div>
-    //       <div className="about-contact">
-    //         Contact
-    //         <ul className="contact-list">
-    //           <li className="contact">
-    //             <a href="mailto:annabrisland@gmail.com">Email</a>
-    //           </li>
-    //           <li className="contact">
-    //             <a href="https://github.com/annabrisland" target="_blank">
-    //               GitHub
-    //             </a>
-    //           </li>
-    //           <li className="contact">
-    //             <a href="www.linkedin.com/in/anna-brisland" target="_blank">
-    //               Linkedin
-    //             </a>
-    //           </li>
-    //         </ul>
-    //       </div>
-    //     </div>
-    //   </div>
-    // </div>
     <header>
       <div className="header-items">
         <a href="/">
