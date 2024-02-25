bb4f80ccc0142831bee82f0e354dc7dafde52bc1
Anna Brisland <84166964+annabrisland@users.noreply.github.com>
2024-02-24 17:33:27 -0800

    Feature/footer (#3)
    * Adds footer template
    
    * Adds scroll effect for footer
    
    * Polish footer container styling
    
    * Tidies footer css
    
    * Adds scroll to footer

--- webapp/src/components/Header.js
+++ webapp/src/components/Header.js
@@ -4,104 +4,49 @@ import "../assets/css/header.css";
 import resume from "../assets/Anna_Brisland_Resume.pdf";
 import { useState } from "react";
 
-export default function Header({ isMobile }) {
-
+export default function Header({ isMobile, handleScroll }) {
   let currentPage = "home";
   if (window.document.URL.includes("about")) {
     currentPage = "about";
-  } else if (window.document.URL.includes("contact")){
-    currentPage = "contact"
-  };
+  } else if (window.document.URL.includes("contact")) {
+    currentPage = "contact";
+  }
 
-  const [ activeLink, setActiveLink ] = useState(currentPage);
- 
+  const activeLink = currentPage;
+
   if (isMobile) {
     <nav className="floating">
-          <ul className="header-menu-list">
-            <li className="header-menu-item">
-              <a className={`header-menu-link ${activeLink == "home" ? "header-menu-current" : ""}`} href="/" >
-                Home
-              </a>
-            </li>
-            <li className="header-menu-item">
-              <a className={`header-menu-link ${activeLink == "about" ? "header-menu-current" : ""}`} href="/about" >
-                About
-              </a>
-            </li>
-            <li className="header-menu-item">
-              <a className={`header-menu-link ${activeLink == "contact" ? "header-menu-current" : ""}`} href="/contact" >
-                Contact
-              </a>
-            </li>
-            <li className="header-menu-item">
-              <a className="header-menu-link" href={resume} target="_blank">
-                Resume
-              </a>
-            </li>
-          </ul>
-        </nav>
+      <ul className="header-menu-list">
+        <li className="header-menu-item">
+          <a
+            className={`header-menu-link ${
+              activeLink === "home" ? "header-menu-current" : ""
+            }`}
+            href="/"
+          >
+            Home
+          </a>
+        </li>
+        <li className="header-menu-item">
+          <a
+            className={`header-menu-link ${
+              activeLink === "contact" ? "header-menu-current" : ""
+            }`}
+            href="/contact"
+          >
+            Contact
+          </a>
+        </li>
+        <li className="header-menu-item">
+          <a className="header-menu-link" href={resume} target="_blank">
+            Resume
+          </a>
+        </li>
+      </ul>
+    </nav>;
   }
 
   return (
-    // <div>
-    //   <nav className="main-header">
-    //     <div className="intro">
-    //       <img className="name-logo" src={nameLogo}></img>
-    //       <p className="name-title">Web Designer & Developer</p>
-    //     </div>
-    //     <div className="nav-items">
-    //       <div className="nav-item about" id="about" onClick={openOverlay}>
-    //         About
-    //       </div>
-    //       <div className="nav-item CTA">Resume</div>
-    //     </div>
-    //   </nav>
-    //   <div className="overlay" id="about-overlay">
-    //     <button className="close-btn" onClick={closeOverlay}></button>
-    //     <div className="overlay-content content grid">
-    //       <div className="about-paragraph">
-    //         <div className="main-paragraph">
-    //           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspisse
-    //           dictum ex non purus cursus tempor.
-    //         </div>
-    //         <div className="second-paragraph">
-    //           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspisse
-    //           dictum ex non purus cursus tempor.
-    //         </div>
-    //       </div>
-    //       <div className="about-skills">
-    //         Skills
-    //         <ul className="skills-list">
-    //           <li className="skill">HTML</li>
-    //           <li className="skill">CSS</li>
-    //           <li className="skill">JavaScript</li>
-    //           <li className="skill">JQuery</li>
-    //           <li className="skill">Bootstrap</li>
-    //           <li className="skill">Figma</li>
-    //           <li className="skill">Adobe Creative Cloud</li>
-    //         </ul>
-    //       </div>
-    //       <div className="about-contact">
-    //         Contact
-    //         <ul className="contact-list">
-    //           <li className="contact">
-    //             <a href="mailto:annabrisland@gmail.com">Email</a>
-    //           </li>
-    //           <li className="contact">
-    //             <a href="https://github.com/annabrisland" target="_blank">
-    //               GitHub
-    //             </a>
-    //           </li>
-    //           <li className="contact">
-    //             <a href="www.linkedin.com/in/anna-brisland" target="_blank">
-    //               Linkedin
-    //             </a>
-    //           </li>
-    //         </ul>
-    //       </div>
-    //     </div>
-    //   </div>
-    // </div>
     <header>
       <div className="header-items">
         <a href="/">
@@ -110,17 +55,22 @@ export default function Header({ isMobile }) {
         <nav className="floating">
           <ul className="header-menu-list">
             <li className="header-menu-item">
-              <a className={`header-menu-link ${activeLink == "home" ? "header-menu-current" : ""}`} href="/" >
+              <a
+                className={`header-menu-link ${
+                  activeLink === "home" ? "header-menu-current" : ""
+                }`}
+                href="/"
+              >
                 Home
               </a>
             </li>
             <li className="header-menu-item">
-              <a className={`header-menu-link ${activeLink == "about" ? "header-menu-current" : ""}`} href="/about" >
-                About
-              </a>
-            </li>
-            <li className="header-menu-item">
-              <a className={`header-menu-link ${activeLink == "contact" ? "header-menu-current" : ""}`} href="/contact" >
+              <a
+                className={`header-menu-link ${
+                  activeLink === "contact" ? "header-menu-current" : ""
+                }`}
+                onClick={handleScroll}
+              >
                 Contact
               </a>
             </li>
